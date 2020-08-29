667415c56b6cd7cd282f7e004957f2af6b4d9bca
Andrew Rogers <andrew.rogerstech@gmail.com>
2020-08-29 14:31:48 +0100

    Tidy up code - remove near duplicate setup code.

--- PiLightMeter/Decimator/aduplex.c
+++ PiLightMeter/Decimator/aduplex.c
@@ -21,57 +21,76 @@
 #include <poll.h>
 #include <alsa/asoundlib.h>
 
-#define PCM_DEVICE "plughw:1"
+#define PCM_DEVICE "hw:1"
 
-int setup(snd_pcm_t *pcm_handle, snd_pcm_hw_params_t *hw_params)
+struct device {
+    snd_pcm_t *handle;
+    snd_pcm_hw_params_t *hw_params;
+    struct pollfd fd;
+};
+
+int setup_device(struct device* dev, char* dev_name, snd_pcm_stream_t stream)
 {
     int err=0;
     unsigned int rate = 96000;
     snd_pcm_format_t format = SND_PCM_FORMAT_S16_LE;
     snd_pcm_uframes_t period = 512;  // Get poll event every 512 frames
-    
 
-    if ((err = snd_pcm_hw_params_any (pcm_handle, hw_params)) < 0) {
+    if ((err = snd_pcm_open(&(dev->handle), dev_name, stream, SND_PCM_NONBLOCK)) < 0) {
+        fprintf (stderr, "Cannot open device %s (%s)\n", dev_name, snd_strerror (err));
+        return (err);
+    }
+
+    // Allocate and initialise parameters.
+    snd_pcm_hw_params_alloca(&(dev->hw_params));
+
+    if ((err = snd_pcm_hw_params_any (dev->handle, dev->hw_params)) < 0) {
         fprintf (stderr, "Canot set parameters (%s)\n", snd_strerror (err));
         return (err);
     }
 
-    if ((err = snd_pcm_hw_params_set_access (pcm_handle, hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
+    if ((err = snd_pcm_hw_params_set_access (dev->handle, dev->hw_params, SND_PCM_ACCESS_RW_INTERLEAVED)) < 0) {
         fprintf (stderr, "Cannot set interleaved (%s)\n", snd_strerror (err));
         return (err);
     }
 
-    if ((err = snd_pcm_hw_params_set_rate_near (pcm_handle, hw_params, &rate, 0)) < 0) {
+    if ((err = snd_pcm_hw_params_set_rate_near (dev->handle, dev->hw_params, &rate, 0)) < 0) {
         fprintf (stderr, "Cannot set rate (%s)\n", snd_strerror (err));
         return (err);
     }
 
-    if ((err = snd_pcm_hw_params_set_format (pcm_handle, hw_params, format)) < 0) {
+    if ((err = snd_pcm_hw_params_set_format (dev->handle, dev->hw_params, format)) < 0) {
         fprintf (stderr, "Cannot set format (%s)\n", snd_strerror (err));
         return (err);
     }
 
-    if ((err = snd_pcm_hw_params_set_channels (pcm_handle, hw_params, 2)) < 0) {
+    if ((err = snd_pcm_hw_params_set_channels (dev->handle, dev->hw_params, 2)) < 0) {
         fprintf (stderr, "Cannot set stereo (%s)\n", snd_strerror (err));
         return (err);
     }
 
-    if ((err = snd_pcm_hw_params (pcm_handle, hw_params)) < 0) {
+    if ((err = snd_pcm_hw_params (dev->handle, dev->hw_params)) < 0) {
         fprintf (stderr, "Cannot apply parameters (%s)\n", snd_strerror (err));
         return (err);
     }
 
-    if((err = snd_pcm_hw_params_set_period_size(pcm_handle, hw_params, period, 0)) < 0) {
+    if((err = snd_pcm_hw_params_set_period_size(dev->handle, dev->hw_params, period, 0)) < 0) {
         fprintf (stderr, "Cannot set period size (%s)\n", snd_strerror (err));
         return (err);
     }
 
-	if ((err = snd_pcm_prepare (pcm_handle)) < 0) {
+	if ((err = snd_pcm_prepare (dev->handle)) < 0) {
         fprintf (stderr, "Cannot prepare  (%s)\n", snd_strerror (err));
         return (err);
     }
 
-    return err;
+    if((err = snd_pcm_poll_descriptors(dev->handle, &(dev->fd), 1)) < 0)
+	{
+		fprintf (stderr, "Cannot get descriptor (%s)\n", snd_strerror (err));
+        return (err);
+	}
+
+	return err;
 }
 
 int playback(snd_pcm_t *pcm_handle, char* buffer, int num_frames)
@@ -109,59 +128,11 @@ int capture(snd_pcm_t *pcm_handle, char* buffer, int num_frames)
     return nfr;
 }
 
-struct device {
-    snd_pcm_t *handle;
-    snd_pcm_hw_params_t *hw_params;
-    struct pollfd fd;
-};
-
-int setup_playback(struct device* dev, char* dev_name)
-{
-    int err=0;
-    if ((err = snd_pcm_open(&(dev->handle), dev_name, SND_PCM_STREAM_PLAYBACK, SND_PCM_NONBLOCK)) < 0) {
-        fprintf (stderr, "Cannot open device %s (%s)\n", dev_name, snd_strerror (err));
-        return (err);
-    }
-
-    // Allocate and initialise parameters.
-    snd_pcm_hw_params_alloca(&(dev->hw_params));
-    if( (err=setup(dev->handle, dev->hw_params)) !=0 ) return(err);
-
-    if((err = snd_pcm_poll_descriptors(dev->handle, &(dev->fd), 1)) < 0)
-	{
-		fprintf (stderr, "Cannot get descriptor (%s)\n", snd_strerror (err));
-        return (err);
-	}
-
-	return err;
-}
-
-int setup_capture(struct device* dev, char* dev_name)
-{
-    int err=0;
-    if ((err = snd_pcm_open (&(dev->handle), dev_name, SND_PCM_STREAM_CAPTURE, SND_PCM_NONBLOCK)) < 0) {
-        fprintf (stderr, "Cannot open device %s (%s)\n", dev_name, snd_strerror (err));
-        return (err);
-    }	   
-
-    // Allocate and initialise parameters.
-    snd_pcm_hw_params_alloca(&(dev->hw_params));
-    if( (err=setup(dev->handle, dev->hw_params)) !=0 ) return(err);
-
-    if((err = snd_pcm_poll_descriptors(dev->handle, &(dev->fd), 1)) < 0)
-	{
-		fprintf (stderr, "Cannot get descriptor (%s)\n", snd_strerror (err));
-        return (err);
-	}
-
-    return err;
-}
-
 int do_playback()
 {
     int err=0;
 	struct device dev;
-	if((err = setup_playback(&dev, PCM_DEVICE)) < 0) {
+	if((err = setup_device(&dev, PCM_DEVICE, SND_PCM_STREAM_PLAYBACK)) < 0) {
 	    fprintf (stderr, "Cannot setup playback device (%s)\n", snd_strerror (err));
 	    return err;
 	}
@@ -180,7 +151,7 @@ int do_playback()
     snd_pcm_close(dev.handle);
 
     free(buffer);
-    
+
     return err;
 }
 
@@ -188,7 +159,7 @@ int do_capture(int N)
 {
     int err=0;
     struct device dev;
-    if((err = setup_capture(&dev, PCM_DEVICE)) < 0) {
+    if((err = setup_device(&dev, PCM_DEVICE, SND_PCM_STREAM_CAPTURE)) < 0) {
 	    fprintf (stderr, "Cannot setup capture device (%s)\n", snd_strerror (err));
 	    return err;
 	}
@@ -214,39 +185,42 @@ int do_both()
 {
     int err=0;
     struct device cdev;
-    if((err = setup_capture(&cdev, PCM_DEVICE)) < 0) {
+    if((err = setup_device(&cdev, PCM_DEVICE, SND_PCM_STREAM_CAPTURE)) < 0) {
 	    fprintf (stderr, "Cannot setup capture device (%s)\n", snd_strerror (err));
 	    return err;
 	}
-	
+
 	struct device pdev;
-	if((err = setup_playback(&pdev, PCM_DEVICE)) < 0) {
+	if((err = setup_device(&pdev, PCM_DEVICE, SND_PCM_STREAM_PLAYBACK)) < 0) {
 	    fprintf (stderr, "Cannot setup playback device (%s)\n", snd_strerror (err));
 	    return err;
 	}
-	
+
 	int num_frames = 1024;
-    char *pbuffer = malloc(num_frames*4); // Each frames has 4 bytes, 2 * 16bit
     char *cbuffer = malloc(num_frames*4); // Each frames has 4 bytes, 2 * 16bit
-    
+    char *pbuffer = malloc(num_frames*4); // Each frames has 4 bytes, 2 * 16bit
+
     struct pollfd fds[2];
     fds[0] = cdev.fd;
     fds[1] = pdev.fd;
 
-    int nfw = playback(pdev.handle, pbuffer, num_frames);
-    int nfr = capture(cdev.handle, cbuffer, num_frames);
-    int nfw_total = 0;
-    if( nfw > 0 ) nfw_total = nfw;
     int nfr_total = 0;
+    int nfr = capture(cdev.handle, cbuffer, num_frames);
     if( nfr > 0 ) nfr_total = nfr;
+
+    int nfw_total = 0;
+    int nfw = playback(pdev.handle, pbuffer, num_frames);
+    if( nfw > 0 ) nfw_total = nfw;
 
     int done=0;
     while ( !done ) {
         poll(fds, 2, 1000);
 
         nfr = 0;
-        if(fds[0].revents) nfr = capture(cdev.handle, cbuffer, num_frames);
-        if( nfr > 0 ) nfr_total += nfr;
+        if(fds[0].revents) {
+            nfr = capture(cdev.handle, cbuffer, num_frames);
+            if( nfr > 0 ) nfr_total += nfr;
+        }
 
         nfw = 0;
         if(fds[1].revents) {
@@ -263,17 +237,19 @@ int do_both()
         poll(fds, 1, 1000);
 
         nfr = 0;
-        if(fds[0].revents) nfr = capture(cdev.handle, cbuffer, remaining > num_frames ? num_frames : remaining);
-        if( nfr > 0 ) remaining -= nfr;
+        if(fds[0].revents) {
+            nfr = capture(cdev.handle, cbuffer, remaining > num_frames ? num_frames : remaining);
+            if( nfr > 0 ) remaining -= nfr;
+        }
     }
 
-    free(pbuffer);
     free(cbuffer);
+    free(pbuffer);
 
+    snd_pcm_close(cdev.handle);
     snd_pcm_nonblock(pdev.handle,0);
     snd_pcm_drain(pdev.handle);
     snd_pcm_close(pdev.handle);
-    snd_pcm_close (cdev.handle);
 }
 
 int main (int argc, char *args[])
