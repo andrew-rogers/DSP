ee801abad09437ea845c46ccafcb1fe21c31962c
Andrew Rogers <andrew.rogerstech@gmail.com>
2021-03-21 18:53:51 +0000

    C++ DAC bitstream interleaver.

--- prbs1bit/prbs1bit.cpp
+++ prbs1bit/prbs1bit.cpp
@@ -19,22 +19,68 @@
 
 #include "prbs1bit.h"
 
+#include <stdlib.h>
+
+namespace
+{
+    uint8_t bitcnt(uint8_t value)
+    {
+        /* Count number of bits that are set in value */
+        int8_t cnt=0;
+        if(value&1) cnt++;
+        if(value&2) cnt++;
+        if(value&4) cnt++;
+        if(value&8) cnt++;
+        if(value&16) cnt++;
+        if(value&32) cnt++;
+        if(value&64) cnt++;
+        if(value&128) cnt++;
+        return cnt;
+    }
+}
+
 Dec8::Dec8()
 {
 
     // Populate the LUT
     for( int i = 0; i<255; i++ ) {
-        int8_t cnt=0;
-        if(i&1) cnt++;
-        if(i&2) cnt++;
-        if(i&4) cnt++;
-        if(i&8) cnt++;
-        if(i&16) cnt++;
-        if(i&32) cnt++;
-        if(i&64) cnt++;
-        if(i&128) cnt++;
-        m_lut[i]=cnt;
+        m_lut[i]=bitcnt(i);
     }
     m_lut[255]=(uint8_t)8;
+}
+
+Int8::Int8()
+{
+    m_rand_index = 0;
+    m_rand280 = 0;
+
+    // Populate the random number table
+    for( int i = 0; i<65536; i++ ) {
+        m_rand_tab[i] = rand()%256;
+    }
+
+    /* Tables of length 280 are used to store combinations with an integer
+       number of repeats.
+
+    | num bits set in byte | num combinations | 280 / num combinations |
+    |          0,8         |        1         |           280          |
+    |          1,7         |        8         |           35           |
+    |          2,6         |        28        |           10           |
+    |          3,5         |        56        |           5            |
+    |          4           |        70        |           4            |
+    */
+
+    uint16_t repeat_tab[9]={280,35,10,5,4,5,10,35,280};
+    uint8_t comb_cnt[9]={0,0,0,0,0,0,0,0,0};
+    for( int value = 0; value<256; value++ ) {
+        int8_t cnt=bitcnt( value );
+
+        /* Get number of repeats and populate bit sequence table */
+        uint16_t nr = repeat_tab[cnt];
+        uint16_t step = 280/nr;
+        uint8_t offset = comb_cnt[cnt];
+        comb_cnt[cnt] = comb_cnt[cnt] + 1;
+        for( int j = 0; j<nr; j++) m_bs_tab[cnt][j*step+offset] = value;
+    }
 }
 
--- prbs1bit/prbs1bit.h
+++ prbs1bit/prbs1bit.h
@@ -26,9 +26,33 @@ class Dec8
 {
 public:
     Dec8();
-    inline uint8_t sample(uint8_t x){ return m_lut[x];}
+    inline uint8_t sample(uint8_t x){ return m_lut[x]; }
 private:
     uint8_t m_lut[256];
+};
+
+/* Takes values 0 to 8 inclusive from stdin and encodes then into a bitstream.
+   When the bitstream is lowpass filtered, this could be considered as a
+   9-level quantiser. Levels 1 to 7 can have different possible bitstreams so
+   these are randomly chosen. */
+class Int8
+{
+public:
+    Int8();
+    inline uint8_t sample(uint8_t in)
+    {
+        if(in>8) in=8;
+        uint8_t out = m_bs_tab[in][m_rand280];
+        m_rand280 = (m_rand280 + m_rand_tab[m_rand_index++]) % 280;
+        return out;
+    }
+private:
+    /* Tables are used for fast processing. */
+    uint8_t m_rand_tab[65536];
+    uint8_t m_bs_tab[9][280];
+    
+    uint16_t m_rand_index;
+    uint16_t m_rand280;
 };
 
 #endif // PRBS1BIT_H
